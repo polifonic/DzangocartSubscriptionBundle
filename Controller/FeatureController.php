b0463279623b882003111b6790438e89e10a80dc
sabir <sabir@crossovernepal.com>
2014-06-06 09:20:11 +0545

    modified feature delete action

--- Controller/FeatureController.php
+++ Controller/FeatureController.php
@@ -78,7 +78,7 @@ class FeatureController extends Controller
         $entity = $this->getQuery()
             ->findPk($id);
 
-        if ($entity && $entity->getPlanFeatures()->toArray() == null) {
+        if ($entity && $entity->getPlanFeatures()->isEmpty()) {
             $entity->delete(); 
         }
 
