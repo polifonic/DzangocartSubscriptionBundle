25a09ccb77330080e99a3942e08dd2b9b9b55555
Olivier Pichon <op@united-asian.com>
2014-06-09 01:00:31 +0700

    Updated bower and nom configs

---
+++ Resources/config/gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require("gulp");
+var bowerFiles = require("gulp-bower-files");
+var uglify = require("gulp-uglify");
+var jsmin = require("gulp-jsmin");
+var rename = require("gulp-rename");
+
+gulp.task("bower-files", function() {
+	 bowerFiles()
+		.pipe(gulp.dest("../public/vendor"));
+});
+
+gulp.task("compress", function() {
+	gulp.src(["../public/vendor/**/*.js", "!../public/vendor/**/*min.js"])
+		.pipe(uglify())
+		.pipe(rename({suffix: ".min"}))
+		.pipe(gulp.dest("../public/vendor"));
+});
+
+gulp.task("minify", function() {
+	gulp.src(["../public/vendor/**/*.js", "!../public/vendor/**/*min.js"])
+		.pipe(jsmin())
+		.pipe(rename({suffix: ".min"}))
+		.pipe(gulp.dest("../public/vendor"));
+});
