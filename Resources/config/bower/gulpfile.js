75afff7ab78a5b89bf9273b7e85675f170b3b0f0
Olivier Pichon <op@united-asian.com>
2014-06-08 22:12:54 +0700

    updated gulpfile.js

--- Resources/config/bower/gulpfile.js
+++ Resources/config/bower/gulpfile.js
@@ -1,6 +1,23 @@
 var gulp = require("gulp");
 var gulpBowerFiles = require("gulp-bower-files");
+var uglify = require('gulp-uglify');
+var jsmin = require('gulp-jsmin');
+var rename = require('gulp-rename');
 
 gulp.task("bower-files", function() {
-     gulpBowerFiles().pipe(gulp.dest("../../public/vendor"));
+	 gulpBowerFiles()
+		.pipe(gulp.dest("../../public/vendor"));
+});
+
+gulp.task('compress', function() {
+	gulp.src('../../public/vendor/**/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest("../../public/vendor"));
+});
+
+gulp.task('minify', function() {
+	gulp.src('../../public/vendor/**/*.js')
+		.pipe(jsmin())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('../../public/vendor'));
 });
