6e2864900ac159122bbc5fb1db37c1b07f8a098b
sabir <sabir@crossovernepal.com>
2014-06-09 17:15:01 +0545

    overrided constructor of unit and plan

--- Propel/Period.php
+++ Propel/Period.php
@@ -6,4 +6,9 @@ use Dzangocart\Bundle\SubscriptionBundle\Propel\om\BasePeriod;
 
 class Period extends BasePeriod
 {
+    function __construct($locale = 'en')
+    {
+        parent::__construct();
+        $this->setLocale($locale);
+    }
 }
--- Propel/Unit.php
+++ Propel/Unit.php
@@ -6,4 +6,9 @@ use Dzangocart\Bundle\SubscriptionBundle\Propel\om\BaseUnit;
 
 class Unit extends BaseUnit
 {
+    function __construct($locale = 'en')
+    {
+        parent::__construct();
+        $this->setLocale($locale);
+    }
 }
