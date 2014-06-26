dac722df9e9466c3ac94f44fd283f6cc6ad97ba7
sabir <sabir@crossovernepal.com>
2014-06-26 09:25:11 +0545

    refactored subscription behavior

--- Propel/Behavior/SubscriptionBehavior.php
+++ Propel/Behavior/SubscriptionBehavior.php
@@ -4,7 +4,6 @@ namespace Dzangocart\Bundle\SubscriptionBundle\Propel\Behavior;
 
 use Behavior;
 use ForeignKey;
-use PropelTypes;
 
 class SubscriptionBehavior extends Behavior
 {
@@ -20,20 +19,12 @@ class SubscriptionBehavior extends Behavior
                 'name' => $this->getParameter('plan_id_column'),
                 'type' => 'INTEGER'
             ));
-        } else {
-            $plan_id_column = $this->getTable()->getColumn($this->getParameter('plan_id_column'));
 
-            if (!(strtoupper($plan_id_column->getType()) == 'INTEGER' )) {
-                $this->getTable()
-                    ->removeColumn($plan_id_column);
-
-                $plan_id_column->setType(PropelTypes::INTEGER);
-                $plan_id_column->setSize(null);
-
-                $this->getTable()
-                    ->addColumn($plan_id_column);
-            }
-
+            $fk = new ForeignKey();
+            $fk->setForeignTableCommonName('plan');
+            $fk->setOnDelete(ForeignKey::RESTRICT);
+            $fk->addReference($this->getParameter('plan_id_column'), 'id');
+            $this->getTable()->addForeignKey($fk);
         }
 
         if (!$this->getTable()->containsColumn($this->getParameter('expires_at_column'))) {
@@ -41,25 +32,6 @@ class SubscriptionBehavior extends Behavior
                 'name' => $this->getParameter('expires_at_column'),
                 'type' => 'TIMESTAMP'
             ));
-        } else {
-            $expires_at_column = $this->getTable()->getColumn($this->getParameter('expires_at_column'));
-
-            if (!(strtoupper($expires_at_column->getType()) == 'TIMESTAMP')) {
-                $this->getTable()
-                    ->removeColumn($expires_at_column);
-
-                $expires_at_column->setType(PropelTypes::TIMESTAMP);
-                $expires_at_column->setSize(null);
-
-                $this->getTable()->addColumn($expires_at_column);
-            }
-
         }
-
-        $fk = new ForeignKey();
-        $fk->setForeignTableCommonName('plan');
-        $fk->setOnDelete(ForeignKey::RESTRICT);
-        $fk->addReference($this->getParameter('plan_id_column'), 'id');
-        $this->getTable()->addForeignKey($fk);
     }
 }
