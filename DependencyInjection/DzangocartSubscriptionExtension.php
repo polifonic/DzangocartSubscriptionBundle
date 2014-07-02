8c2d902cbe7a3b51dd801b4ece48eaf2afc60b70
sabir <sabir@crossovernepal.com>
2014-07-02 12:44:47 +0545

    added plan controller as service and loaded service.yml via dependency injection

--- DependencyInjection/DzangocartSubscriptionExtension.php
+++ DependencyInjection/DzangocartSubscriptionExtension.php
@@ -2,7 +2,9 @@
 
 namespace Dzangocart\Bundle\SubscriptionBundle\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class DzangocartSubscriptionExtension extends Extension
@@ -16,5 +18,7 @@ class DzangocartSubscriptionExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $container->setParameter('dzangocart_subscription.pricing.theme', $config['pricing']['theme']);
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
     }
 }
