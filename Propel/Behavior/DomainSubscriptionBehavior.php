15b1d939968c44df2a86f77cf0b633e053dbabb7
sabir <sabir@crossovernepal.com>
2014-07-07 17:17:48 +0545

    refactored domain subscription beg=havior

--- Propel/Behavior/DomainSubscriptionBehavior.php
+++ Propel/Behavior/DomainSubscriptionBehavior.php
@@ -7,10 +7,10 @@ use Behavior;
 class DomainSubscriptionBehavior extends Behavior
 {
     protected $had_domain_column = 1;
+    protected $had_custom_column = 1;
 
     protected $parameters = array(
         'domain_column' => 'domain',
-        'host_column' => 'host',
         'custom_column' => 'custom'
     );
 
@@ -26,23 +26,50 @@ class DomainSubscriptionBehavior extends Behavior
             $this->had_domain_column = 0;
         }
 
-        if (!$this->getTable()->containsColumn($this->getParameter('host_column'))) {
+        if (!$this->getTable()->containsColumn($this->getParameter('custom_column'))) {
             $this->getTable()->addColumn(array(
-                'name' => $this->getParameter('host_column'),
+                'name' => $this->getParameter('custom_column'),
                 'type' => 'VARCHAR',
-                'size' => '100'
+                'size' => '132'
             ));
+            $this->had_custom_column = 0;
         }
     }
 
     public function objectMethods(PHP5ObjectBuilder $builder)
     {
-        if (!$this->had_domain_column) {
-
-            if ($this->getTable()->containsColumn($this->getParameter('custom_column'))) {
+        if ($this->had_domain_column && $this->had_custom_column) {
                 $name_for_get_custom = $this->getTable()->getColumn($this->getParameter('custom_column'))->getPhpName();
-
+                $name_for_get_domain = $this->getTable()->getColumn($this->getParameter('domain_column'))->getPhpName();
                 return '
+/**
+ * Returns the fully qualified hostname of the subscription account
+ */
+public function getHostname($host)
+{
+    if (!$this->get' . $name_for_get_custom . '() == null ) {
+        return $this->get' . $name_for_get_custom . '();
+    } else {
+        return $this->get' . $name_for_get_domain . '().\'.\'.$host;
+    }
+}';
+        } elseif ($this->had_domain_column && !$this->had_custom_column) {
+            $name_for_get_domain = $this->getTable()->getColumn($this->getParameter('domain_column'))->getPhpName();
+            return '
+/**
+ * Returns the fully qualified hostname of the subscription account
+ */
+public function getHostname($host)
+{
+    if (!$this->get' . ucfirst($this->getParameter('custom_column')) . '() == null ) {
+        return $this->get' . ucfirst($this->getParameter('custom_column')) . '();
+    } else {
+        return $this->get' . $name_for_get_domain . '().\'.\'.$host;
+    }
+}';
+        } elseif (!$this->had_domain_column && $this->had_custom_column) {
+            $name_for_get_custom = $this->getTable()->getColumn($this->getParameter('custom_column'))->getPhpName();
+            return '
 /**
  * Returns the fully qualified hostname of the subscription account
  */
@@ -54,16 +81,19 @@ public function getHostname($host)
         return $this->get' . ucfirst($this->getParameter('domain_column')) . '().\'.\'.$host;
     }
 }';
-            } else {
-                return '
+        } else {
+            return '
 /**
  * Returns the fully qualified hostname of the subscription account
  */
 public function getHostname($host)
 {
-    return $this->get' . ucfirst($this->getParameter('domain_column')) . '().\'.\'.$host;
-}';
-            }
-        }
+    if (!$this->get' . ucfirst($this->getParameter('custom_column')) . '() == null ) {
+        return $this->get' . ucfirst($this->getParameter('custom_column')) . '();
+    } else {
+        return $this->get' . ucfirst($this->getParameter('domain_column')) . '().\'.\'.$host;
     }
+}';
+        }
+    }  
 }
