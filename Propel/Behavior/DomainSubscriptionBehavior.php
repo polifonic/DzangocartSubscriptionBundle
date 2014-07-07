682e7154281d878a6ce9d6c20fd63b57a372be97
sabir <sabir@crossovernepal.com>
2014-07-07 11:07:01 +0545

    refactored domain subscription behavior

--- Propel/Behavior/DomainSubscriptionBehavior.php
+++ Propel/Behavior/DomainSubscriptionBehavior.php
@@ -33,17 +33,31 @@ class DomainSubscriptionBehavior extends Behavior
     }
 
     public function objectMethods(PHP5ObjectBuilder $builder)
-    { 
+    {
+        $custom_array = explode('_', $this->getParameter('custom_column'));
+        $name_for_get_custom = '';
+
+        foreach ($custom_array as $x) {
+            $name_for_get_custom = $name_for_get_custom . ucfirst($x);
+        }
+
+        $domain_array = explode('_', $this->getParameter('domain_column'));
+        $name_for_get_domain = '';
+        
+        foreach ($domain_array as $x) {
+            $name_for_get_domain = $name_for_get_domain . ucfirst($x);
+        }
+
     return '
 /**
  * Returns the fully qualified hostname of the subscription account
  */
 public function getHostname($host)
 {
-    if (!$this->get'.ucfirst($this->getParameter('custom_column')).'() == null ) {
-        return $this->getCustom();
+    if (!$this->get' . $name_for_get_custom . '() == null ) {
+        return $this->get' . $name_for_get_custom . '();
     } else {
-        return $this->get'.ucfirst($this->getParameter('domain_column')).'().\'.\'.$host;
+        return $this->get' . $name_for_get_domain . '().\'.\'.$host;
     }
 }';
     }
