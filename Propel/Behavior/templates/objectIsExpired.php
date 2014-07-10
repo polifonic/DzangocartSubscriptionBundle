5f5ad6ec6cfb5ac8972d6cd520829da2a95f52f2
sabir <sabir@crossovernepal.com>
2014-07-10 10:14:50 +0545

    removed initial spaces

--- Propel/Behavior/templates/objectIsExpired.php
+++ Propel/Behavior/templates/objectIsExpired.php
@@ -1,9 +1,9 @@
 
- /**
- * Whether the subscription is expired
- *
- * @boolean true if the subscription is expired, false otherwise
- */
+/**
+* Whether the subscription is expired
+*
+* @boolean true if the subscription is expired, false otherwise
+*/
 public function isExpired()
 {
     $expire = $this->get<?php echo $column_name ?>('U');
