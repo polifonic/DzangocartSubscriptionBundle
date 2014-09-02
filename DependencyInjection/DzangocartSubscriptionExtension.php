c8bdd19e349f17e234cd1ec76a316a2b15695364
sabir <sabir@crossovernepal.com>
2014-09-02 10:43:17 +0545

    refactored config tree

--- DependencyInjection/DzangocartSubscriptionExtension.php
+++ DependencyInjection/DzangocartSubscriptionExtension.php
@@ -2,6 +2,7 @@
 
 namespace Dzangocart\Bundle\SubscriptionBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
@@ -18,14 +19,25 @@ class DzangocartSubscriptionExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $container->setParameter('dzangocart_subscription.pricing.theme', $config['pricing']['theme']);
-        $container->setParameter('dzangocart_subscription.trial.config', $config['trial']);
 
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config/services'));
         $loader->load('plan.yml');
 
-        if (isset($config['class'])) {
-            $loader->load('form.yml');
-            $container->setParameter('dzangocart_subscription.class', $config['class']);
+        if ($config['signup']['enabled']) {
+            $this->loadSignup($config['signup'], $container, $loader);
         }
+    }
+
+    protected function loadSignup(array $config, ContainerBuilder $container, YamlFileLoader $loader)
+    {
+        if (!isset($config['class'])) {
+            throw new InvalidConfigurationException(
+                'The "dzangocart_subscription.signup.class" configuration class option must be set or "dzangocart_subscription.signup.enabled" must be set to false.'
+            );
+        }
+
+        $container->setParameter('dzangocart_subscription.class', $config['class']);
+        $container->setParameter('dzangocart_subscription.trial.config', $config['trial']);
+        $loader->load('form.yml');
     }
 }
