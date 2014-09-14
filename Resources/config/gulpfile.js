441f7de518180c213b8d2ccb6894bca21163f6c6
Olivier Pichon <op@united-asian.com>
2014-09-14 21:34:59 +0700

    updated bower config and assets

--- Resources/config/gulpfile.js
+++ Resources/config/gulpfile.js
@@ -1,16 +1,22 @@
-var gulp = require("gulp");
-var bowerFiles = require("gulp-bower-files");
-var jsmin = require("gulp-jsmin");
-var rename = require("gulp-rename");
+var gulp = require('gulp');
+var mainBowerFiles = require('main-bower-files');
+var jsmin = require('gulp-jsmin');
+var rename = require('gulp-rename');
 
-gulp.task("bower-files", function() {
-	 bowerFiles()
-		.pipe(gulp.dest("../public/vendor"));
+gulp.task('bower-files', function() {
+	return gulp.src(mainBowerFiles(), { base: 'bower_components' })
+		.pipe(gulp.dest('../public/vendor'));
+
 });
 
-gulp.task("minify", function() {
-	gulp.src(["../public/vendor/**/*.js", "!../public/vendor/**/*min.js"])
+gulp.task('minify', ['bower-files'], function() {
+	return gulp.src([
+			'../public/vendor/datatables-plugins/**/*.js',
+			'!../public/vendor/**/*min.js'
+		], { base: '../public/vendor' })
 		.pipe(jsmin())
-		.pipe(rename({suffix: ".min"}))
-		.pipe(gulp.dest("../public/vendor"));
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('../public/vendor'));
 });
+
+gulp.task('default', ['minify'])
