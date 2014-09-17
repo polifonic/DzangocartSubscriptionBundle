e9f27ac177f0a1aad5fd99825f7bdb0338e38b8a
sabir <sabir@crossovernepal.com>
2014-09-17 13:27:08 +0545

    behavior to add magic call method

--- Propel/Behavior/Subscription/SubscriptionBehaviorObjectBuilderModifier.php
+++ Propel/Behavior/Subscription/SubscriptionBehaviorObjectBuilderModifier.php
@@ -16,6 +16,7 @@ class SubscriptionBehaviorObjectBuilderModifier
         $this->builder = $builder;
         $script = '';
         $script .= $this->addIsExpired();
+        $script .= $this->add__Call();
 
         if (!$this->table->getBehavior('domainsubscription')) {
             $this->builder->declareClasses('Symfony\Component\Validator\Constraints\NotNull');
@@ -37,5 +38,10 @@ class SubscriptionBehaviorObjectBuilderModifier
         return $this->behavior->renderTemplate('objectLoadValidatorMetadata', array(
             'plan_id_column' => $this->table->getBehavior('subscription')->getParameter('plan_id_column')
         ));
+    }
+
+    protected function add__Call()
+    {
+        return $this->behavior->renderTemplate('objectCall', array());
     }
 }
---
+++ Propel/Behavior/Subscription/templates/objectCall.php
@@ -0,0 +1,20 @@
+
+/**
+ * magic method to return wiki's feature value
+ */
+public function __call($name, $params)
+{
+    if (substr($name, 0, 3) == 'get') {
+        $planFeatures = $this->getPlan()->getFeatures();
+
+        foreach ($planFeatures as $planFeature) {
+            $feature = $planFeature->getFeature();
+
+            if ($feature->getName() == substr($name,3)) {
+                return (string) $planFeature;
+            }
+        }
+    }
+
+    throw new PropelException('Call to undefined method: ' . $name);
+}
