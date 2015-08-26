c489b273e78440f7cf7194e5658006b28aa718a5
Olivier Pichon <op@united-asian.com>
2015-08-26 18:34:39 +0700

    fixed behavior

--- Propel/Behavior/SubscriptionBehaviorObjectBuilderModifier.php
+++ Propel/Behavior/SubscriptionBehaviorObjectBuilderModifier.php
@@ -183,7 +183,7 @@ public function __call(\$name, \$params)
         foreach (\$planFeatures as \$planFeature) {
             \$feature = \$planFeature->getFeature();
 
-            if (PropelInflector::camelize(\$feature->getPropertyName()) == PropelInflector::camelize(substr($name, 3))) {
+            if (PropelInflector::camelize(\$feature->getPropertyName()) == PropelInflector::camelize(substr(\$name, 3))) {
                 return (string) \$planFeature;
             }
         }
